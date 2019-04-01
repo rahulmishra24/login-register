f952ea265e04c00d9342fd2db8c3f356fec2d634
Rahul Mishra <mishrarahul215@gmail.com>
2019-04-01 20:57:44 +0530

    mongoDB and session commited

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+mongoose.connect('mongodb://localhost/userdetails');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({path:"/",secret:"nosecretcoden",resave:false,saveUninitialize:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
